85e185b13f8afabb9d62e799dc7afd5088fe190d
Alexander Hoffman <alxhoff@gmail.com>
2024-11-07 16:08:52 +0100

    New fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,18 @@
-set -g theme_color_scheme solarized-dark
-tput cup 62
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+    
+function g
+    # grep -r --color=auto --exclude-dir={.git,node_modules} $argv .
+    grep -n -r --color=auto --exclude=tags --exclude=TAGS --exclude-dir={.git,node_modules} "$argv" .
+end
+
+function f
+    find . -name "$argv"
+end
+
+function t
+    set current_dir (pwd)
+    xfce4-terminal --working-directory="$current_dir" &
+end
+
